411690e3fa5ab0ea220a5d2a85af8998a949aa25
ttruong89 <t.truong2009@gmail.com>
2017-02-24 23:00:40 -0600

    config and models

---
+++ models/burger.js
@@ -0,0 +1,7 @@
+module.exports = function(sequelize, DataTypes) {
+	var Burger = sequelize.define("Burger", {
+		burger_name: DataTypes.STRING
+		devoured: DataTypes.BOOLEAN
+	});
+	return Burger;
+};
